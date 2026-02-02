b7469119fee2418141aedefd95953ffa55ce4060
Richelieu <richelieu042@gmail.com>
2026-02-02 15:03:53 +0800

    init

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"time"
+
 	"github.com/richelieu-yang/UnchartedWaters/src/adb"
 	"github.com/richelieu-yang/chimera/v3/src/log/console"
+	"github.com/richelieu-yang/chimera/v3/src/randomKit"
+	"github.com/richelieu-yang/chimera/v3/src/time/timeKit"
 )
 
 func main() {
-	a := &adb.Adb{
-		Address: "127.0.0.1:5555",
-	}
+	a := adb.New("127.0.0.1:5555", "1920x1080")
 	if err := a.CheckEnv(); err != nil {
 		console.Fatalf("a.CheckEnv() failed: %s", err)
 		return
@@ -16,5 +18,31 @@ func main() {
 	if err := a.Initialize(); err != nil {
 		console.Fatalf("a.Initialize() failed: %s", err)
 		return
+	}
+
+	for {
+		// 随机睡眠500ms ~ 1000ms
+		randInt := randomKit.Int(500, 1001)
+		time.Sleep(time.Millisecond * time.Duration(randInt))
+
+		imgName := "screenshot_" + timeKit.FormatCurrent(timeKit.FormatFileName) + ".png"
+		start := time.Now()
+		if err := a.Screenshot(imgName); err != nil {
+			console.Errorf("a.Screenshot() failed: %s", err)
+			continue
+		}
+		console.Infof("screenshot: %s, cost: %s", imgName, time.Since(start))
+
+		//// adb -s {a.address} exec-out screencap -p
+		//data, err := cmdKit.RunCombinedly(context.TODO(), "adb", "-s", a.address, "exec-out", "screencap", "-p")
+		//if err != nil {
+		//	console.Errorf("cmdKit.RunCombinedly() failed: %s", err)
+		//	continue
+		//}
+		//
+		//err = os.WriteFile(imgName, data, 0644)
+		//if err != nil {
+		//	panic(err)
+		//}
 	}
 }
--- src/adb/structs.go
+++ src/adb/structs.go
@@ -2,21 +2,44 @@ package adb
 
 import (
 	"context"
+	"os"
+	"regexp"
+	"sync"
 
 	"github.com/richelieu-yang/chimera/v3/src/command/cmdKit"
 	"github.com/richelieu-yang/chimera/v3/src/core/errorKit"
+	"github.com/richelieu-yang/chimera/v3/src/core/intKit"
+	"github.com/richelieu-yang/chimera/v3/src/file/fileKit"
 	"github.com/richelieu-yang/chimera/v3/src/log/console"
 )
 
 type Adb struct {
-	// Address 安卓设备的地址
+	screenshotMutex *sync.Mutex
+
+	// address 安卓设备的地址
 	/*
 		（1）基本语法：adb connect <设备IP地址>:<端口号>
 		（2）默认端口号是 5555，如果使用默认端口，可以省略端口号
 			e.g. adb connect 192.168.1.100 <=> adb connect 192.168.1.100:5555
 		（3）mac上，BlueStacks Air默认是 "127.0.0.1:5555"
 	*/
-	Address string
+	address string
+
+	// specifiedPhysicalSize 指定的物理尺寸（显示分辨率）
+	/*
+		（1）如果不一致，CheckEnv 会返回 error
+		（2）如果为空，则不检查
+		e.g. "1920x1080"
+	*/
+	specifiedPhysicalSize string
+}
+
+func New(address string, specifiedPhysicalSize string) *Adb {
+	return &Adb{
+		screenshotMutex:       &sync.Mutex{},
+		address:               address,
+		specifiedPhysicalSize: specifiedPhysicalSize,
+	}
 }
 
 func (a *Adb) CheckEnv() error {
@@ -24,7 +47,7 @@ func (a *Adb) CheckEnv() error {
 	if err != nil {
 		return errorKit.Wrapf(err, "fail to look path of adb")
 	}
-	console.Infof("adb path: %s", path)
+	console.Infof("adb path: [%s]", path)
 
 	// adb 版本号
 	{
@@ -59,11 +82,12 @@ func (a *Adb) Initialize() error {
 		return errorKit.Wrapf(err, "fail to run 'adb start-server'")
 	}
 
-	// adb connect {a.Address}
-	_, err = cmdKit.RunCombinedlyToString(context.TODO(), "adb", "connect", a.Address)
+	// adb connect {a.address}
+	_, err = cmdKit.RunCombinedlyToString(context.TODO(), "adb", "connect", a.address)
 	if err != nil {
-		return errorKit.Wrapf(err, "fail to run 'adb connect %s'", a.Address)
+		return errorKit.Wrapf(err, "fail to run 'adb connect %s'", a.address)
 	}
+	console.Infof("Connect to [%s] successfully.", a.address)
 
 	// adb devices
 	devices, err := cmdKit.RunCombinedlyToString(context.TODO(), "adb", "devices")
@@ -71,6 +95,63 @@ func (a *Adb) Initialize() error {
 		return errorKit.Wrapf(err, "fail to run 'adb devices'")
 	}
 	console.Infof("adb devices:\n%s", devices)
+
+	return nil
+}
+
+// GetPhysicalSize 获取：分辨率（宽高、尺寸）.
+func (a *Adb) GetPhysicalSize() (width int, height int, err error) {
+	// 执行命令：adb -s 127.0.0.1:5555 shell wm size
+	str, err := cmdKit.RunCombinedlyToString(context.TODO(), "adb", "-s", a.address, "shell", "wm", "size")
+	if err != nil {
+		return 0, 0, errorKit.Wrapf(err, "fail to run 'adb -s %s shell wm size'", a.address)
+	}
+
+	/*
+		e.g. "Physical size: 1920x1080"
+			matches[0] = "1920x1080" — 完整匹配，即整个正则表达式匹配到的完整字符串
+			matches[1] = "1920" — 第一个捕获组，对应第一个括号 (\d+) 匹配的内容
+			matches[2] = "1080" — 第二个捕获组，对应第二个括号 (\d+) 匹配的内容
+	*/
+	re := regexp.MustCompile(`(\d+)x(\d+)$`)
+	matches := re.FindStringSubmatch(str)
+	if matches == nil {
+		return 0, 0, errorKit.Newf("fail to get physical size from [%s]", str)
+	}
+	width, err = intKit.StringToInt(matches[1])
+	if err != nil {
+		return 0, 0, errorKit.Wrapf(err, "fail to get width from [%s]", matches[1])
+	}
+	height, err = intKit.StringToInt(matches[2])
+	if err != nil {
+		return 0, 0, errorKit.Wrapf(err, "fail to get height from [%s]", matches[2])
+	}
+	return
+}
+
+// Screenshot 截图
+/*
+	@param targetPath: 截图保存的路径
+*/
+func (a *Adb) Screenshot(targetPath string) error {
+	a.screenshotMutex.Lock()
+	defer a.screenshotMutex.Unlock()
+
+	// adb -s {a.address} exec-out screencap -p
+	data, err := cmdKit.RunCombinedly(context.TODO(), "adb", "-s", a.address, "exec-out", "screencap", "-p")
+	if err != nil {
+		return errorKit.Wrapf(err, "fail to run 'adb -s %s exec-out screencap -p'", a.address)
+	}
+
+	// 尝试创建父目录
+	if err := fileKit.MkParentDirs(targetPath); err != nil {
+		return errorKit.Wrapf(err, "fail to make parent dirs of [%s]", targetPath)
+	}
+
+	err = os.WriteFile(targetPath, data, 0644)
+	if err != nil {
+		return errorKit.Wrapf(err, "fail to write file [%s]", targetPath)
+	}
 
 	return nil
 }
