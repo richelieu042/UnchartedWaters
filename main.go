c4da922939d4607ba3a94c853e01227ae3324b26
Richelieu <richelieu042@gmail.com>
2026-03-13 14:49:44 +0800

    init

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"os"
-
 	"github.com/richelieu-yang/UnchartedWaters/src/cmd"
 )
 
 func main() {
 	/* test */
-	{
-		addr := "127.0.0.1:5555"
-		//addr := "127.0.0.1:5585"
-		//os.Args = []string{"uw", "--addr", addr, "--clean", "--verbose"}
-		os.Args = []string{"uw", "--addr", addr, "--clean"}
-	}
+	//{
+	//	addr := "127.0.0.1:5555"
+	//	//addr := "127.0.0.1:5585"
+	//	//os.Args = []string{"uw", "--addr", addr, "--clean", "--verbose"}
+	//	os.Args = []string{"uw", "--addr", addr, "--clean"}
+	//}
 
 	if err := cmd.Execute(); err != nil {
 		panic(err)
