29354af73e25f990b7a018d3e4662b2d2c32f378
Richelieu <richelieu042@gmail.com>
2026-03-18 13:51:53 +0800

    init

--- src/logic/battling.go
+++ src/logic/battling.go
@@ -1,8 +1,12 @@
 package logic
 
 import (
+	"time"
+
 	"github.com/richelieu042/chimera/v3/src/android/adbKit"
+	"github.com/richelieu042/chimera/v3/src/concurrency/rateLimitKit"
 	"github.com/richelieu042/chimera/v3/src/core/error/errKit"
+	"go.uber.org/ratelimit"
 	"go.uber.org/zap"
 )
 
@@ -22,5 +26,38 @@ func isBattling(logger *zap.Logger, imgPath string) (bool, error) {
 }
 
 func processBattling(adbClient adbKit.Client, l *zap.Logger, imgPath string) {
+	// 限流器：避免短时间内操作太多次
+	limiter := rateLimitKit.NewUberLimiter(1, ratelimit.Per(500*time.Millisecond), ratelimit.WithoutSlack)
 
+	// (1) 召唤海军
+	{
+		op := "navy"
+		templPath := "images/sail/navy.png"
+
+		flag := matchAndTap(adbClient, l, op, imgPath, templPath, limiter)
+		switch flag {
+		case 0, 2:
+			return
+		case 1, 3:
+			// 继续向下走
+		default:
+			l.Panic("Unknown flag.", zap.String("op", op), zap.Int("flag", flag))
+		}
+	}
+
+	// (2) 召唤副舰
+	{
+		op := "assistant"
+		templPath := "images/sail/assistant.png"
+
+		flag := matchAndTap(adbClient, l, op, imgPath, templPath, limiter)
+		switch flag {
+		case 0, 2:
+			return
+		case 1, 3:
+			// 继续向下走
+		default:
+			l.Panic("Unknown flag.", zap.String("op", op), zap.Int("flag", flag))
+		}
+	}
 }
