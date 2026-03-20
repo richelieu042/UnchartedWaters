64aa8c6ce354c857b58eac3e82c4c2ac2c7d2eb8
Richelieu <richelieu042@gmail.com>
2026-03-20 13:51:56 +0800

    init

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"os"
 
 	"github.com/richelieu-yang/UnchartedWaters/src/cmd"
+	"github.com/richelieu042/chimera/v3/src/core/sliceKit"
+	"github.com/richelieu042/chimera/v3/src/log/console"
 )
 
 func main() {
 	/* test */
 	{
-		//addr := "192.168.60.205:5555"
-		addr := "192.168.60.205:16384"
-		//addr := "127.0.0.1:5555"
+		//addr := "192.168.60.205:16384"
+		addr := "127.0.0.1:5555"
 		//addr := "127.0.0.1:5585"
 
 		os.Args = []string{"uw", "--addr", addr, "--clean"}
@@ -28,6 +29,8 @@ func main() {
 		if disableBattle {
 			os.Args = append(os.Args, "--disable_battle")
 		}
+
+		console.Warnf("command: %s", sliceKit.Join(os.Args, " "))
 	}
 
 	if err := cmd.Execute(); err != nil {
